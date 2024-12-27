71a7cbe8372fc862a00772fdc01cbbc9fb779b91
Meghashyam12 <86647274+Meghashyam12@users.noreply.github.com>
2024-12-28 01:17:56 +0530

    implemented import and made functions pure

---
+++ commands.js
@@ -0,0 +1,165 @@
+export const EMPTYOBJECT = {};
+
+const toObject = (array) => {
+  const obj = {};
+  obj[array[0]] = array[1];
+  return obj;
+};
+
+const createTable = (_, name, columns, __, isExistingTable) => {
+  if (isExistingTable) {
+    console.log("Could not create table, name already exists");
+    return EMPTYOBJECT;
+  }
+  if (columns.length === 0) {
+    console.log("Invalid argument length for columns");
+    return EMPTYOBJECT;
+  }
+
+  const seperatedCols = columns[0].split(",").map((col) => col.split(":"));
+  const colsAsObjects = seperatedCols.map(toObject);
+  console.log("Succesfully created table " + name);
+
+  return { tableName: name, columns: colsAsObjects, rows: [] };
+};
+
+const isNumber = (table, key) => {
+  const column = table.columns.filter(
+    (column) => Object.keys(column)[0] === key
+  );
+  return column[0][key] === "number";
+};
+
+const isNaN = (number) => {
+  return "" + number === "NaN" && number !== "NaN";
+};
+
+const isMatchingDatatype = (table, key, value) => {
+  const column = table.columns.filter(
+    (column) => Object.keys(column)[0] === key
+  );
+
+  return column[0][key] === typeof value && !isNaN(value);
+};
+
+const createRow = (table) => {
+  return (obj, value, index) => {
+    const key = Object.keys(table.columns[index])[0];
+    obj[key] = isNumber(table, key) ? +value : value;
+
+    return isMatchingDatatype(table, key, obj[key]) ? obj : {};
+  };
+};
+
+const insertIntoTable = (originalTable, name, values) => {
+  const splitValues = values[0].split(",");
+
+  if (splitValues.length !== originalTable.columns.length) {
+    console.log("Could not insert values, invalid argument length");
+    return EMPTYOBJECT;
+  }
+
+  const pushedValues = splitValues.reduce(createRow(originalTable), {});
+
+  if (Object.keys(pushedValues).length !== splitValues.length) {
+    console.log("Data type mismatch for arguments");
+    return EMPTYOBJECT;
+  }
+
+  const resultTable = { ...originalTable };
+  resultTable.rows.push(pushedValues);
+  console.log("Succesfully inserted " + values + " into " + name + " table");
+
+  return resultTable;
+};
+
+const comparators = {
+  ">": (num1, num2) => num1 > num2,
+  ">=": (num1, num2) => num1 >= num2,
+  "<": (num1, num2) => num1 < num2,
+  "<=": (num1, num2) => num1 <= num2,
+  "==": (num1, num2) => num1 == num2,
+  "===": (num1, num2) => num1 == num2,
+  "!=": (num1, num2) => num1 != num2,
+  "!==": (num1, num2) => num1 != num2,
+};
+
+function getPredicate(condition) {
+  let predicate = () => true;
+  if (condition) {
+    const operands = condition.split(" ");
+    const key = operands[0];
+    const comparator = operands[1];
+    const value = operands[2];
+
+    predicate = (row) => {
+      return comparators[comparator](row[key], value);
+    };
+  }
+
+  return predicate;
+}
+
+const selectFromTable = (originalTable, tableName, _, condition) => {
+  const predicate = getPredicate(condition);
+  const selectedRows = originalTable.rows.filter(predicate);
+
+  console.log(
+    "Row(s) fetched: " + selectedRows.length + " from " + tableName + " table"
+  );
+  console.table(selectedRows);
+
+  return EMPTYOBJECT;
+};
+
+const updateTable = (originalTable, tableName, args, condition) => {
+  const key = args[0];
+  const value = args[1];
+  const predicate = getPredicate(condition);
+  const resultTable = { ...originalTable };
+  let updateCount = 0;
+
+  for (const row of resultTable.rows) {
+    if (predicate(row)) {
+      row[key] = isNumber(resultTable, key) ? +value : value;
+      updateCount += 1;
+    }
+  }
+
+  console.log(updateCount + " row(s) updated in " + tableName + " table");
+  return resultTable;
+};
+
+const deleteRow = (originalTable, tableName, _, condition) => {
+  const predicate = getPredicate(condition);
+  const resultTable = { ...originalTable };
+  let index = 0;
+  let deleteCount = 0;
+
+  while (index < resultTable.rows.length) {
+    if (predicate(resultTable.rows[index])) {
+      resultTable.rows.splice(index, index + 1);
+      index -= 1;
+      deleteCount += 1;
+    }
+
+    index++;
+  }
+
+  console.log(deleteCount + " row(s) deleted from " + tableName + " table");
+  return resultTable;
+};
+
+const truncateTable = (originalTable, tableName) => {
+  console.log("Truncated " + tableName + " table succesfully");
+  return deleteRow(originalTable, tableName, "", "");
+};
+
+export const commands = {
+  CREATE: createTable,
+  INSERT: insertIntoTable,
+  SELECT: selectFromTable,
+  UPDATE: updateTable,
+  DELETE: deleteRow,
+  TRUNCATE: truncateTable,
+};
--- prequel.js
+++ prequel.js
@@ -1,228 +1,94 @@
+import { commands } from "./commands.js";
+import { EMPTYOBJECT } from "./commands.js";
+
 const myDB = {
   tables: [],
 };
 
-let currentDB = myDB;
+// const anotherDB = {  //future implementation
+//   tables: [],
+// };
 
-function showDataBase() {
+const currentDB = myDB;
+
+const showDataBase = (database) => {
+  console.log("DATABASE:");
   console.log("*".repeat(10));
-  console.table(myDB.tables);
+  console.table(database.tables);
   console.log("*".repeat(10));
-}
-
-const isMatchingTable = (tableName) => {
-  return (element) => element.tableName === tableName;
 };
 
-const getTableLocation = (database, tableName) =>
-  database.tables.findIndex(isMatchingTable(tableName));
-
-function doesTableExist(database, tableName) {
-  return getTableLocation(database, tableName) >= 0;
-}
-
-const toObject = (array) => {
-  const obj = {};
-  obj[array[0]] = array[1];
-  return obj;
-};
-
-const createTable = (database, name, columns) => {
-  if (doesTableExist(database, name)) {
-    console.log("Could not create table, name already exists");
-    return;
-  }
-
-  if (!columns) {
-    console.log("Invalid argument length for columns");
-    return;
-  }
-
-  const seperatedCols = columns.split(",").map((col) => col.split(":"));
-  const colsAsObjects = seperatedCols.map(toObject);
-  database.tables.push({ tableName: name, columns: colsAsObjects, rows: [] });
-  console.log("Succesfully created table", name);
-};
-
-const dropTable = (database, tableName) => {
-  const tableIndex = getTableLocation(database, tableName);
-  database.tables.splice(tableIndex, tableIndex + 1);
-
+const dropTable = (database, tableName, tableLocation) => {
+  database.tables.splice(tableLocation, tableLocation + 1);
   console.log(tableName, "table dropped");
 };
 
-const isNumber = (table, key) => {
-  const column = table.columns.filter(
-    (column) => Object.keys(column)[0] === key
-  );
-  return column[0][key] === "number";
-};
-
-const isNaN = (number) => {
-  return "" + number === "NaN" && number !== "NaN";
-};
-
-const isMatchingDatatype = (table, key, value) => {
-  const column = table.columns.filter(
-    (column) => Object.keys(column)[0] === key
-  );
-
-  return column[0][key] === typeof value && !isNaN(value);
-};
-
-const createRow = (table) => {
-  return (obj, value, index) => {
-    const key = Object.keys(table.columns[index])[0];
-    obj[key] = isNumber(table, key) ? +value : value;
-
-    return isMatchingDatatype(table, key, obj[key]) ? obj : {};
-  };
-};
-
-const insertIntoTable = (database, tableName, values) => {
-  const table = database.tables[getTableLocation(database, tableName)];
-  const splitValues = values.split(",");
-
-  if (splitValues.length !== table.columns.length) {
-    console.log("Could not insert values, invalid argument length");
-    return;
-  }
-
-  const pushedValues = splitValues.reduce(createRow(table), {});
-
-  if (Object.keys(pushedValues).length !== splitValues.length) {
-    console.log("Data type mismatch for arguments");
-    return;
-  }
-
-  database.tables[getTableLocation(database, tableName)].rows.push(
-    pushedValues
-  );
-
-  console.log("Values inserted succesfully");
-};
-
-const comparators = {
-  ">": (num1, num2) => num1 > num2,
-  ">=": (num1, num2) => num1 >= num2,
-  "<": (num1, num2) => num1 < num2,
-  "<=": (num1, num2) => num1 <= num2,
-  "==": (num1, num2) => num1 == num2,
-  "===": (num1, num2) => num1 == num2,
-  "!=": (num1, num2) => num1 != num2,
-  "!==": (num1, num2) => num1 != num2,
-};
-
-function getPredicate(condition) {
-  let predicate = () => true;
-  if (condition) {
-    const operands = condition.split(" ");
-    const key = operands[0];
-    const comparator = operands[1];
-    const value = operands[2];
-
-    predicate = (row) => {
-      return comparators[comparator](row[key], value);
-    };
-  }
-
-  return predicate;
-}
-
-const selectFromTable = (database, tableName, condition) => {
-  const predicate = getPredicate(condition);
-  const table = database.tables[getTableLocation(database, tableName)];
-  const selectedRows = table.rows.filter(predicate);
-
-  console.log("Rows fetched:", selectedRows.length);
-  console.table(selectedRows);
-};
-
-const updateTable = (database, tableName, key, value, condition) => {
-  const predicate = getPredicate(condition);
-  const table = database.tables[getTableLocation(database, tableName)];
-  let updateCount = 0;
-
-  for (const row of table.rows) {
-    if (predicate(row)) {
-      row[key] = isNumber(table, key) ? +value : value;
-      updateCount += 1;
-    }
-  }
-
-  console.log(updateCount + "rows updated");
-};
-
-const deleteRow = (database, tableName, condition) => {
-  const predicate = getPredicate(condition);
-  const table = database.tables[getTableLocation(database, tableName)];
-  let index = 0;
-  let deleteCount = 0;
-
-  while (index < table.rows.length) {
-    if (predicate(table.rows[index])) {
-      table.rows.splice(index, index + 1);
-      index -= 1;
-      deleteCount += 1;
-    }
-
-    index++;
-  }
-
-  console.log(deleteCount + " rows deleted");
-};
-
-const truncateTable = (database, tableName) => {
-  deleteRow(database, tableName, "");
-  console.log("Truncated " + tableName + " table succesfully");
-};
-
-showDataBase(myDB);
-
-const commands = {
-  CREATE: createTable,
-  DROP: dropTable,
-  INSERT: insertIntoTable,
-  SELECT: selectFromTable,
-  UPDATE: updateTable,
-  DELETE: deleteRow,
+const internalCommands = {
   SHOWDB: showDataBase,
-  TRUNCATE: truncateTable,
+  DROP: dropTable,
 };
 
-const breakQuery = (query) => {
-  const whereSplit = query.split("WHERE ");
-  const args = whereSplit[0].trimEnd().split(" ");
-  const command = args[0];
-  const condition = whereSplit[1];
-
-  return [args, command, condition];
-};
-
-function validateQuery(database, args, command) {
+function validateQuery(database, tableName, _, command) {
   if (command === "" || command === "EXIT") {
     return false;
   }
 
-  if (!(command in commands)) {
+  if (!(command in commands) && !(command in internalCommands)) {
     console.log("Invalid Command");
     return false;
   }
 
-  if (
-    command !== "CREATE" &&
-    command !== "SHOWDB" &&
-    !doesTableExist(database, args[1])
-  ) {
-    console.log(args[1] + " table does not exist");
+  const doesTableExist =
+    database.tables.findIndex(isMatchingTable(tableName)) !== -1;
+
+  if (command !== "CREATE" && command !== "SHOWDB" && !doesTableExist) {
+    console.log(tableName + " table does not exist");
     return false;
   }
 
   return true;
 }
 
-const executeQuery = (database, args, command, condition) => {
-  commands[command](database, ...args.slice(1, args.length), condition);
+const isMatchingTable = (tableName) => {
+  return (element) => element.tableName === tableName;
+};
+
+const executeQuery = (database, tableName, args, command, condition) => {
+  const tableLocation = database.tables.findIndex(isMatchingTable(tableName));
+
+  if (command in internalCommands) {
+    internalCommands[command](database, tableName, tableLocation);
+    return;
+  }
+
+  const originalTable = database.tables[tableLocation];
+  const isExistingTable = tableLocation !== -1;
+  const newTable = commands[command](
+    originalTable,
+    tableName,
+    args,
+    condition,
+    isExistingTable
+  );
+
+  if (!isExistingTable && newTable !== EMPTYOBJECT) {
+    database.tables.push(newTable);
+    return;
+  }
+  if (newTable !== EMPTYOBJECT) {
+    database.tables[tableLocation] = newTable;
+    return;
+  }
+};
+
+const breakQuery = (query) => {
+  const whereSplit = query.split("WHERE ");
+  const args = whereSplit[0].trimEnd().split(" ");
+  const command = args[0];
+  const tableName = args[1];
+  const condition = whereSplit[1];
+
+  return [tableName, args.slice(2, args.length), command, condition];
 };
 
 let query = "";
